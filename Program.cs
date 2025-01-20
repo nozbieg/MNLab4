4c199077bb2e6aa0b1f04f28a4a1301ef33c7ceb
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2025-01-20 22:47:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using static MNLab4.RungeKuttaSolver;
+
+Console.WriteLine("Choose function F(x, y):");
+Console.WriteLine("1: F(x, y) = a * x + b * y");
+Console.WriteLine("2: F(x, y) = a * x - b * y");
+Console.WriteLine("3: F(x, y) = a * x * b * y");
+Console.WriteLine("4: F(x, y) = (a * x) / (b * y)");
+int choice = int.Parse(Console.ReadLine());
+
+Func<double, double, double, double, double> F = choice switch
+{
+    1 => F1,
+    2 => F2,
+    3 => F3,
+    4 => F4,
+    _ => throw new ArgumentException("Invalid choice!")
+};
+
+Console.Write("Enter x0: ");
+double x0 = double.Parse(Console.ReadLine());
+Console.Write("Enter y0: ");
+double y0 = double.Parse(Console.ReadLine());
+Console.Write("Enter step size h: ");
+double h = double.Parse(Console.ReadLine());
+Console.Write("Enter number of steps n: ");
+int n = int.Parse(Console.ReadLine());
+
+double a, b;
+while (true)
+{
+    Console.Write("Enter constant a (non-zero): ");
+    a = double.Parse(Console.ReadLine());
+    if (a != 0) break;
+    Console.WriteLine("Constant a cannot be zero! Try again.");
+}
+
+while (true)
+{
+    Console.Write("Enter constant b (non-zero): ");
+    b = double.Parse(Console.ReadLine());
+    if (b != 0) break;
+    Console.WriteLine("Constant b cannot be zero! Try again.");
+}
+
+Console.WriteLine("\nChoose method:");
+Console.WriteLine("1: Runge-Kutta 3rd order");
+Console.WriteLine("2: Runge-Kutta 4th order");
+int methodChoice = int.Parse(Console.ReadLine());
+
+(List<double> xVals, List<double> yVals) results;
+string methodName;
+
+if (methodChoice == 1)
+{
+    methodName = "Runge-Kutta 3rd order";
+    results = RungeKutta3(x0, y0, h, n, F, a, b);
+}
+else if (methodChoice == 2)
+{
+    methodName = "Runge-Kutta 4th order";
+    results = RungeKutta4(x0, y0, h, n, F, a, b);
+}
+else
+{
+    Console.WriteLine("Invalid choice!");
+    return;
+}
+
+PlotResults(results.xVals, results.yVals, methodName);
---
+++ RungeKuttaSolver.cs
@@ -0,0 +1,103 @@
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MNLab4;
+public class RungeKuttaSolver
+{
+    public static double F1(double x, double y, double a, double b)
+    {
+        return a * x + b * y;
+    }
+
+    public static double F2(double x, double y, double a, double b)
+    {
+        return a * x - b * y;
+    }
+
+    public static double F3(double x, double y, double a, double b)
+    {
+        return a * x * b * y;
+    }
+
+    public static double F4(double x, double y, double a, double b)
+    {
+        return y != 0 ? (a * x) / (b * y) : 0;
+    }
+
+    public static (List<double> xVals, List<double> yVals) RungeKutta3(double x0, double y0, double h, int n, Func<double, double, double, double, double> F, double a, double b)
+    {
+        var xVals = new List<double> { x0 };
+        var yVals = new List<double> { y0 };
+
+        for (int i = 0; i < n; i++)
+        {
+            double xi = xVals.Last();
+            double yi = yVals.Last();
+            double m1 = F(xi, yi, a, b);
+            double m2 = F(xi + h / 2, yi + (h / 2) * m1, a, b);
+            double m3 = F(xi + h, yi - h * m1 + 2 * h * m2, a, b);
+            double yiPlus1 = yi + (h / 6) * (m1 + 4 * m2 + m3);
+            double xiPlus1 = xi + h;
+            xVals.Add(xiPlus1);
+            yVals.Add(yiPlus1);
+        }
+
+        return (xVals, yVals);
+    }
+
+    public static (List<double> xVals, List<double> yVals) RungeKutta4(double x0, double y0, double h, int n, Func<double, double, double, double, double> F, double a, double b)
+    {
+        var xVals = new List<double> { x0 };
+        var yVals = new List<double> { y0 };
+
+        for (int i = 0; i < n; i++)
+        {
+            double xi = xVals.Last();
+            double yi = yVals.Last();
+            double m1 = F(xi, yi, a, b);
+            double m2 = F(xi + h / 2, yi + (h / 2) * m1, a, b);
+            double m3 = F(xi + h / 2, yi + (h / 2) * m2, a, b);
+            double m4 = F(xi + h, yi + h * m3, a, b);
+            double yiPlus1 = yi + (h / 6) * (m1 + 2 * m2 + 2 * m3 + m4);
+            double xiPlus1 = xi + h;
+            xVals.Add(xiPlus1);
+            yVals.Add(yiPlus1);
+        }
+
+        return (xVals, yVals);
+    }
+
+    public static void PlotResults(List<double> xVals, List<double> yVals, string methodName)
+    {
+        Console.WriteLine($"\nWyniki u¿ywaj¹c {methodName}:");
+        for (int i = 0; i < xVals.Count; i++)
+        {
+            Console.WriteLine($"x = {xVals[i]:F4}, y = {yVals[i]:F4}");
+        }
+
+        var plotModel = new PlotModel { Title = $"Rozwi¹zanie równania metod¹ {methodName}" };
+        var lineSeries = new LineSeries
+        {
+            Title = methodName,
+            MarkerType = MarkerType.Circle
+        };
+
+        for (int i = 0; i < xVals.Count; i++)
+        {
+            lineSeries.Points.Add(new DataPoint(xVals[i], yVals[i]));
+        }
+
+        plotModel.Series.Add(lineSeries);
+
+        using (var stream = new System.IO.FileStream($"{methodName}.png", System.IO.FileMode.Create))
+        {
+            PngExporter.Export(plotModel, stream,800,600);
+        }
+
+        Console.WriteLine($"Wykres zapisany jako {methodName}.png");
+    }
+}
